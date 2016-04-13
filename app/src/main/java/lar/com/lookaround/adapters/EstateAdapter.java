a3c93227cd8255d82f24d5891007e2d547f68182
Attila Dan <dn.attila@icloud.com>
2016-04-13 14:19:58 +0200

    ugybizonydaaaaa

--- app/src/main/java/lar/com/lookaround/adapters/EstateAdapter.java
+++ app/src/main/java/lar/com/lookaround/adapters/EstateAdapter.java
@@ -1,15 +1,20 @@
 package lar.com.lookaround.adapters;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,12 +53,42 @@ public class EstateAdapter extends ArrayAdapter<RealEstate> {
         description.setText(estate.getDescription());
         price.setText(estate.getPrice());
         fav.setChecked(estate.isFavourite());
-        loadEstateImages(convertView, estate.getUrls());
+        //loadEstateImages(convertView, estate.getUrls());
+        new DownloadImageTask((ImageView) convertView.findViewById(R.id.item_realestate_mainpic))
+                .execute(estate.getUrls());
+
+
 
         return convertView;
     }
 
-    public void loadEstateImages(View view, String url) {
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+            bmImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }
+    }
+
+    /*public void loadEstateImages(View view, String url) {
         SliderLayout sliderLayout = (SliderLayout) view.findViewById(R.id.sliderSmall);
 
 
@@ -66,13 +101,7 @@ public class EstateAdapter extends ArrayAdapter<RealEstate> {
                     @Override
                     public void onSliderClick(BaseSliderView slider) {
                         //Log.d("CLICKED ON: ", SmallURLS.get(finalI));
-                        /*if(sliderLayout.getScaleX() == 1) {
-                               sliderLayout.setScaleY(0.5f);
-                               sliderLayout.setScaleX(0.5f);
-                           } else {
-                               sliderLayout.setScaleY(1);
-                               sliderLayout.setScaleX(1);
-                           }*/
+
                     }
                 });
         sliderLayout.stopAutoCycle();
@@ -81,7 +110,7 @@ public class EstateAdapter extends ArrayAdapter<RealEstate> {
         sliderLayout.addSlider(defaultSliderView);
 
 
-    }
+    }*/
 
 
 }
