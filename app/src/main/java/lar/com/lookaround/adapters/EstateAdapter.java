53bed9867641da5632b79e166634eb6829008766
Attila Dan <dn.attila@icloud.com>
2016-04-18 14:43:34 +0200

    ViewPagerIndicator beginned, AsyncTask perfected

--- app/src/main/java/lar/com/lookaround/adapters/EstateAdapter.java
+++ app/src/main/java/lar/com/lookaround/adapters/EstateAdapter.java
@@ -96,13 +96,9 @@ public class EstateAdapter extends ArrayAdapter<RealEstate> {
         //imageTask.execute(estate.getUrls());
 
         final DownloadImageTask task = new DownloadImageTask(image, position, convertView);
-        //final AsyncDrawable asyncDrawable = new AsyncDrawable(task);
 
         imageList.add(task);
-
-        //image.setImageDrawable(asyncDrawable);
         task.execute(estate.getUrls());
-
 
         return convertView;
     }
@@ -172,29 +168,6 @@ public class EstateAdapter extends ArrayAdapter<RealEstate> {
         }
     }
 
-    static class AsyncDrawable extends BitmapDrawable {
-        private final WeakReference<DownloadImageTask> bitmapWorkerTaskReference;
-
-        public AsyncDrawable(DownloadImageTask bitmapWorkerTask) {
-            //super(res, bitmap);
-            bitmapWorkerTaskReference = new WeakReference<DownloadImageTask>(bitmapWorkerTask);
-        }
-
-        public DownloadImageTask getBitmapWorkerTask() {
-            return bitmapWorkerTaskReference.get();
-        }
-    }
-
-    private static DownloadImageTask getBitmapWorkerTask(ImageView imageView) {
-        if (imageView != null) {
-            final Drawable drawable = imageView.getDrawable();
-            if (drawable instanceof AsyncDrawable) {
-                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
-                return asyncDrawable.getBitmapWorkerTask();
-            }
-        }
-        return null;
-    }
 
 
 
