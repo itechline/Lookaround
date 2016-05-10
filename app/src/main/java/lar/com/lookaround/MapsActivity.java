f346af08434cbd83234e40ca49b167daff7d5f4f
Attila Dan <dn.attila@icloud.com>
2016-05-10 14:49:12 +0200

    sok sákség befejezve

--- app/src/main/java/lar/com/lookaround/MapsActivity.java
+++ app/src/main/java/lar/com/lookaround/MapsActivity.java
@@ -1,6 +1,7 @@
 package lar.com.lookaround;
 
 import android.Manifest;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Address;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -53,12 +55,18 @@ public class MapsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setTitle("My Map");
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                setUpMap(googleMap);
+                mMap = googleMap;
+                setUpClusterer();
+
+                //setUpMap(googleMap);
+
             }
         });
     }
@@ -87,6 +95,17 @@ public class MapsActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        /*if(viewFlip.getDisplayedChild() == ESTATESLIST) {
+            getMenuInflater().inflate(R.menu.main, menu);
+        }*/
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     private void setUpMap(GoogleMap map) {
         mMap = map;
 
@@ -100,4 +119,180 @@ public class MapsActivity extends AppCompatActivity {
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
         }
     }
+
+    private String mSnippet;
+    private String mTitle;
+
+    public class MyItem implements ClusterItem {
+        private final LatLng mPosition;
+
+        public MyItem(double lat, double lng, String t, String s) {
+            mPosition = new LatLng(lat, lng);
+            mTitle = t;
+            mSnippet = s;
+        }
+
+        @Override
+        public LatLng getPosition() {
+            return mPosition;
+        }
+
+        public String getTitle(){
+            return mTitle;
+        }
+
+        public String getSnippet(){
+            return mSnippet;
+        }
+
+
+    }
+
+
+    private ClusterManager<MyItem> mClusterManager;
+
+    private void setUpClusterer() {
+        // Declare a variable for the cluster manager.
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        mClusterManager = new ClusterManager<>(this, mMap);
+        mMap.setInfoWindowAdapter(mClusterManager.getMarkerManager());
+
+
+        mClusterManager.setOnClusterClickListener(new ClusterManager.OnClusterClickListener<MyItem>() {
+            @Override
+            public boolean onClusterClick(final Cluster<MyItem> cluster) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                cluster.getPosition(), (float) Math.floor(mMap
+                                        .getCameraPosition().zoom + 1)), 300,
+                        null);
+                return true;
+            }
+        });
+
+        mClusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<MyItem>() {
+            @Override
+            public boolean onClusterItemClick(MyItem clusterItem) {
+                clickedClusterItem = clusterItem;
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                clusterItem.getPosition(), (float) Math.floor(mMap
+                                        .getCameraPosition().zoom + 1)), 300,
+                        null);
+                return false;
+            }
+        });
+
+
+
+        //mClusterManager.setOnClusterItemInfoWindowClickListener(this); //added
+
+
+        mMap.setOnCameraChangeListener(mClusterManager);
+        mMap.setOnMarkerClickListener(mClusterManager);
+
+
+        mMap.setInfoWindowAdapter(mClusterManager.getMarkerManager());
+        mMap.setOnInfoWindowClickListener(mClusterManager);
+
+
+
+
+        // Add cluster items (markers) to the cluster manager.
+        //addItems();
+
+        for (int i = 0; i < 13; i++) {
+            try {
+                Log.d("ADD_CLUSTER ", "TRY");
+                getLocate("Debrecen Kassai " + String.valueOf(i));
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("CLUSTER_ERROR: ", e.toString());
+            }
+        }
+
+        mClusterManager.getMarkerCollection().setOnInfoWindowAdapter(new MyCustomAdapterForItems());
+    }
+
+
+    private MyItem clickedClusterItem;
+    public class MyCustomAdapterForItems implements GoogleMap.InfoWindowAdapter {
+
+        private final View myContentsView;
+
+        MyCustomAdapterForItems() {
+            myContentsView = getLayoutInflater().inflate(R.layout.maps_info_window, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+
+            TextView tvTitle = ((TextView) myContentsView.findViewById(R.id.item_realestate_adress1_maps));
+            TextView tvSnippet = ((TextView) myContentsView.findViewById(R.id.item_realestate_adress2_maps));
+
+            tvTitle.setText(clickedClusterItem.getTitle());
+            tvSnippet.setText(clickedClusterItem.getSnippet());
+
+            return myContentsView;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            return null;
+        }
+    }
+
+    private void gotoLocation(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mMap.moveCamera(update);
+
+    }
+
+
+    public void getLocate(String adress) throws IOException {
+        Geocoder gc = new Geocoder(this);
+
+        List<Address> list = gc.getFromLocationName(adress, 1);
+        Address add = list.get(0);
+
+        double lat = add.getLatitude();
+        double lng = add.getLongitude();
+
+
+        MyItem offsetItem = new MyItem(lat, lng, "LOFASZ", "LOFASZ2");
+        mClusterManager.addItem(offsetItem);
+        gotoLocation(lat, lng, 10);
+        Log.d("CLUSTER: ", "ADDED");
+
+    }
+
+
+    private void addItems() {
+
+        // Set some lat/lng coordinates to start with.
+        double lat = 51.5145160;
+        double lng = -0.1270060;
+
+        // Add ten cluster items in close proximity, for purposes of this example.
+        for (int i = 0; i < 10; i++) {
+            double offset = i / 60d;
+            lat = lat + offset;
+            lng = lng + offset;
+            MyItem offsetItem = new MyItem(lat, lng, "LOFASZ", "LOFASZ2");
+            mClusterManager.addItem(offsetItem);
+        }
+    }
+
 }
