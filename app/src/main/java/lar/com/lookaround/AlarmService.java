70fa4595c37d31b13383284649555ce031c8d7df
Attila Dan <dn.attila@icloud.com>
2016-07-06 15:31:44 +0200

    notification logo changed

--- app/src/main/java/lar/com/lookaround/AlarmService.java
+++ app/src/main/java/lar/com/lookaround/AlarmService.java
@@ -3,8 +3,8 @@ package lar.com.lookaround;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 
@@ -52,8 +52,7 @@ public class AlarmService extends IntentService {
 
         PendingIntent contentIntent = PendingIntent.getActivity(this, iUniqueId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        int icon = R.mipmap.ic_launcher;
-
+        int icon = R.drawable.bonodom_logo;
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
